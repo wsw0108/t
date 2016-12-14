dd557d38220a8d99ea613a913305e43087ac8026
FuZhenn <fuzhen@maptalks.org>
2016-12-14 23:27:31 +0800

    use Object.getPrototypeOf to traverse proto chains.

--- hook.js
+++ hook.js
@@ -1,63 +1,58 @@
 class A {
-    // static hooks = []
     constructor() {
-        console.log('A', this.constructor.hooks.join())
-        // this.callInitHook()
     }
     static addInitHook(fn) {
-        const args = Array.prototype.slice.call(arguments, 1)
-        const init = typeof fn === 'function' ? fn : function () {
-            this[fn].apply(this, args)
+        var proto = this.prototype;
+        var parentProto = Object.getPrototypeOf(proto);
+        if (!proto._initHooks || proto._initHooks == parentProto._initHooks) {
+            console.log('init hooks array');
+            proto._initHooks = [];
         }
-        if (!this.hooks || !this.hasOwnProperty(this.hooks)) {
-            console.log('init hooks array')
-            this.hooks = []
-        }
-        // this.hooks.push(init)
-        this.hooks.push(fn)
+        proto._initHooks.push(fn);
     }
-    callInitHook() {
-        if (this._initHooksCalled) {
-            return
-        }
-        this._initHooksCalled = true
-        const hooks = this.constructor.hooks
-        for (let i = 0; i < hooks.length; i++) {
-            hooks[i].call(this)
+
+    aMethod() {
+        console.log('aMethod');
+    }
+
+    callInitHooks() {
+        var proto = Object.getPrototypeOf(this);
+        console.log('callInitHooks', proto._initHooks.join());
+
+        if (Object.getPrototypeOf(proto).callInitHooks) {
+            this.callInitHooks.call(proto);
         }
     }
 }
+
 class B extends A {
-    // static hooks = []
     constructor() {
         super()
         this.handlers = []
-        console.log('B', B.hooks.join())   
-    }
-    addHandler(name) {
-        this.handlers.push(name)
+        console.log('B', B.prototype._initHooks.join())
     }
     bMethod() {}
 }
+
 class C extends A {
     constructor() {
         super()
         this.handlers = []
-        console.log('C', C.hooks.join())
+        console.log('C', C.prototype._initHooks.join())
     }
-    addHandler(name) {
-        this.handlers.push(name)
+    aMethod() {
+        console.log('C.aMethod');
+        super.aMethod();
     }
     cMethod() {}
 }
 
+A.addInitHook('addHandler0', 'h0')
 B.addInitHook('addHandler1', 'h1')
 C.addInitHook('addHandler2', 'h2')
 C.addInitHook('addHandler3', 'h3')
-console.log('B.hooks', B.hooks)
-console.log('C.hooks', C.hooks)
 
 const b = new B()
 const c = new C()
-console.log('b.handlers', b.handlers)
-console.log('c.handlers', c.handlers)
+
+c.callInitHooks();
