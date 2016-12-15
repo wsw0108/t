f6ccdded5c3a40dcc1405224f5c71ee87749778b
wsw <wsw0108@gmail.com>
2016-12-15 09:35:00 +0800

    merge https://github.com/wsw0108/t/pull/1

--- hook.js
+++ hook.js
@@ -1,61 +1,73 @@
 class A {
-    // static hooks = []
     constructor() {
-        console.log('A', this.constructor.hooks.join())
-        // this.callInitHook()
+        this.callInitHooks()
     }
     static addInitHook(fn) {
         const args = Array.prototype.slice.call(arguments, 1)
         const init = typeof fn === 'function' ? fn : function () {
             this[fn].apply(this, args)
         }
-        if (!this.hooks || !this.hasOwnProperty(this.hooks)) {
+        const proto = this.prototype
+        const parentProto = Object.getPrototypeOf(proto)
+        if (!proto._initHooks || proto._initHooks === parentProto._initHooks) {
             console.log('init hooks array')
-            this.hooks = []
+            proto._initHooks = []
         }
-        // this.hooks.push(init)
-        this.hooks.push(fn)
+        proto._initHooks.push(init)
     }
-    callInitHook() {
+    callInitHooks() {
+        // TODO: this???
         if (this._initHooksCalled) {
             return
         }
         this._initHooksCalled = true
-        const hooks = this.constructor.hooks
+        const proto = Object.getPrototypeOf(this)
+        const parentProto = Object.getPrototypeOf(proto)
+        if (parentProto.callInitHooks) {
+            parentProto.callInitHooks.call(this)
+        }
+        const hooks = proto._initHooks
         for (let i = 0; i < hooks.length; i++) {
             hooks[i].call(this)
         }
     }
 }
 class B extends A {
-    // static hooks = []
     constructor() {
         super()
-        this.handlers = []
-        console.log('B', B.hooks.join())   
+        // this.handlers = []
+        console.log('B.constructor')
+        console.log(B.prototype._initHooks.join())   
     }
     addHandler(name) {
+        if (!this.handlers) {
+            this.handlers = []
+        }
         this.handlers.push(name)
     }
     bMethod() {}
 }
-class C extends A {
+class C extends B {
     constructor() {
         super()
-        this.handlers = []
-        console.log('C', C.hooks.join())
+        // this.handlers = []
+        console.log('C.constructor')
+        console.log(C.prototype._initHooks.join())
     }
-    addHandler(name) {
+    addHandlerC(name) {
+        if (!this.handlers) {
+            this.handlers = []
+        }
         this.handlers.push(name)
     }
     cMethod() {}
 }
 
-B.addInitHook('addHandler1', 'h1')
-C.addInitHook('addHandler2', 'h2')
-C.addInitHook('addHandler3', 'h3')
-console.log('B.hooks', B.hooks)
-console.log('C.hooks', C.hooks)
+B.addInitHook('addHandler', 'h1')
+C.addInitHook('addHandler', 'h2')
+C.addInitHook('addHandler', 'h3')
+console.log('B.hooks', B.prototype._initHooks)
+console.log('C.hooks', C.prototype._initHooks)
 
 const b = new B()
 const c = new C()
