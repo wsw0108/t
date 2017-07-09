6d6c00cbe7bb35c322f7eb0096161f879976f191
wsw0108 <wsw0108@gmail.com>
2017-07-10 07:53:34 +0800

    update gl-tests

--- gl-tests/parameters.node.js
+++ gl-tests/parameters.node.js
@@ -1,7 +1,8 @@
 const createContext = require('gl')
 
-const gl = createContext(1, 1)
+console.log('gl getParameters: ')
 
+const gl = createContext(1, 1)
 Object.keys(gl).filter(key => key.startsWith('MAX')).forEach(key => {
   const limit = gl.getParameter(gl[key])
   console.log(`${key}: ${limit}`)
