a725aee82bb587a7697a0371233cd0d941fb015c
wsw0108 <wsw0108@gmail.com>
2017-09-08 18:21:57 +0800

    update maptalks-multi.html

--- demo/tile/js/create.js
+++ demo/tile/js/create.js
@@ -1,22 +1,22 @@
-function createMap(container, tileSize, srs) {
-  var urlTemplate = 'http://webrd{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}';
-  var subdomains = ['01','02','03','04'];
-  var attribution = '&copy; <a href="http://www.gaode.com/">Gaode.com</a>';
+var urlTemplate = 'http://webrd{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}';
+var subdomains = ['01','02','03','04'];
+var attribution = '&copy; <a href="http://www.gaode.com/">Gaode.com</a>';
+function createMap(container, params) {
   var baseLayer = new maptalks.TileLayer('base', {
-    urlTemplate: urlTemplate,
-    subdomains: subdomains,
-    tileSize: [tileSize, tileSize]
+    urlTemplate: params.urlTemplate || urlTemplate,
+    subdomains: params.subdomains || subdomains,
+    tileSize: [params.tileSize, params.tileSize]
   });
   var options = {
     center: [121, 31],
     zoom: 13,
     baseLayer: baseLayer,
     attribution: {
-      content: attribution
+      content: params.attribution || attribution
     }
   }
-  if (srs) {
-    options.spatialReference = srs;
+  if (params.srs) {
+    options.spatialReference = params.srs;
   }
   var map = new maptalks.Map(container, options);
   return map;
